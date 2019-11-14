8a671ea1a5e83215d6094fdc6b1acad082e250cb
lideming <14901890+lideming@users.noreply.github.com>
2019-11-15 01:57:02 +0800

    wip: player basic

--- main.js
+++ main.js
@@ -1,40 +1,103 @@
-var helpers = {
-    toggleClass: function (element, clsName, force) {
+var helpers = new /** @class */ (function () {
+    function class_1() {
+        this.Timer = /** @class */ (function () {
+            function class_2(callback) {
+                this.callback = callback;
+            }
+            class_2.prototype.timeout = function (time) {
+                var handle = setTimeout(this.callback, time);
+                this.cancelFunc = function () { return window.clearTimeout(handle); };
+            };
+            class_2.prototype.interval = function (time) {
+                var handle = setInterval(this.callback, time);
+                this.cancelFunc = function () { return window.clearInterval(handle); };
+            };
+            class_2.prototype.tryCancel = function () {
+                if (this.cancelFunc)
+                    this.cancelFunc();
+            };
+            return class_2;
+        }());
+    }
+    class_1.prototype.toggleClass = function (element, clsName, force) {
         if (force === undefined)
             force = !element.classList.contains(clsName);
         if (force)
             element.classList.add(clsName);
         else
             element.classList.remove(clsName);
-    }
-};
+    };
+    class_1.prototype.strPaddingLeft = function (str, len, ch) {
+        if (ch === void 0) { ch = ' '; }
+        while (str.length < len) {
+            str = ch + str;
+        }
+        return str;
+    };
+    class_1.prototype.formatTime = function (sec) {
+        var sec = Math.floor(sec);
+        var min = Math.floor(sec / 60);
+        sec %= 60;
+        return this.strPaddingLeft(min.toString(), 2, '0') + ':' + this.strPaddingLeft(sec.toString(), 2, '0');
+    };
+    class_1.prototype.numLimit = function (num, min, max) {
+        return (num < min || typeof num != 'number' || isNaN(num)) ? min :
+            (num > max) ? max : num;
+    };
+    return class_1;
+}());
 var ui = {
-    bottomBar: {
-        container: document.getElementById("bottombar"),
-        toggle: function (state) {
-            helpers.toggleClass(ui.bottomBar.container, 'show', state);
-        },
-        init: function () {
-            var bar = ui.bottomBar.container;
+    bottomBar: new /** @class */ (function () {
+        function class_3() {
+            this.container = document.getElementById("bottombar");
+        }
+        class_3.prototype.toggle = function (state) {
+            helpers.toggleClass(this.container, 'show', state);
+        };
+        class_3.prototype.init = function () {
+            var _this = this;
+            var bar = this.container;
+            var hideTimer = new helpers.Timer(function () {
+                _this.toggle(false);
+            });
             bar.addEventListener('mouseenter', function () {
-                ui.bottomBar.toggle(true);
+                hideTimer.tryCancel();
+                _this.toggle(true);
             });
             bar.addEventListener('mouseleave', function () {
-                ui.bottomBar.toggle(false);
+                hideTimer.tryCancel();
+                hideTimer.timeout(200);
             });
+        };
+        return class_3;
+    }()),
+    progressBar: new /** @class */ (function () {
+        function class_4() {
+            this.container = document.getElementById('progressbar');
+            this.fill = document.getElementById('progressbar-fill');
+            this.labelCur = document.getElementById('progressbar-label-cur');
+            this.labelTotal = document.getElementById('progressbar-label-total');
         }
-    },
-    progressBar: {
-        container: document.getElementById('progressbar'),
-        fill: document.getElementById('progressbar-fill'),
-        setProg: function (prog) {
-            if (typeof prog != 'number' || prog < 0)
-                prog = 0;
-            else if (prog > 1)
-                prog = 1;
-            ui.progressBar.fill.style.width = (prog * 100) + '%';
-        }
-    }
+        class_4.prototype.setProg = function (cur, total) {
+            var prog = cur / total;
+            prog = helpers.numLimit(prog, 0, 1);
+            this.fill.style.width = (prog * 100) + '%';
+            this.labelCur.textContent = helpers.formatTime(cur);
+            this.labelTotal.textContent = helpers.formatTime(total);
+        };
+        class_4.prototype.setProgressChangedCallback = function (cb) {
+            var _this = this;
+            var call = function (e) { cb(helpers.numLimit(e.offsetX / _this.container.clientWidth, 0, 1)); };
+            this.container.addEventListener('mousedown', function (e) {
+                call(e);
+            });
+            this.container.addEventListener('mousemove', function (e) {
+                if (e.buttons == 1)
+                    call(e);
+            });
+        };
+        return class_4;
+    }())
 };
 ui.bottomBar.init();
 var PlayerCore = /** @class */ (function () {
@@ -42,11 +105,17 @@ var PlayerCore = /** @class */ (function () {
         var _this = this;
         this.audio = document.createElement('audio');
         this.audio.addEventListener('timeupdate', function (e) {
-            ui.progressBar.setProg(_this.audio.currentTime / _this.audio.duration);
+            ui.progressBar.setProg(_this.audio.currentTime, _this.audio.duration);
+        });
+        ui.progressBar.setProgressChangedCallback(function (x) {
+            _this.audio.currentTime = x * _this.audio.duration;
         });
     }
-    PlayerCore.prototype.playUrl = function (src) {
+    PlayerCore.prototype.loadUrl = function (src) {
         this.audio.src = src;
+    };
+    PlayerCore.prototype.playUrl = function (src) {
+        this.loadUrl(src);
         this.audio.play();
     };
     PlayerCore.prototype.play = function () {
@@ -58,9 +127,4 @@ var PlayerCore = /** @class */ (function () {
     return PlayerCore;
 }());
 var playerCore = new PlayerCore();
-window.onkeydown = function (e) {
-    console.log(e);
-    if (e.keyCode == 32) {
-        ui.bottomBar.toggle();
-    }
-};
+playerCore.loadUrl('test.mp3');
--- main.ts
+++ main.ts
@@ -1,34 +1,88 @@
-var helpers = {
-    toggleClass(element: HTMLElement, clsName: string, force?: boolean){
+var helpers = new class {
+    toggleClass(element: HTMLElement, clsName: string, force?: boolean) {
         if (force === undefined) force = !element.classList.contains(clsName);
         if (force) element.classList.add(clsName);
         else element.classList.remove(clsName);
     }
+    strPaddingLeft(str: string, len: number, ch: string = ' ') {
+        while (str.length < len) {
+            str = ch + str;
+        }
+        return str;
+    }
+    formatTime(sec: number) {
+        var sec = Math.floor(sec);
+        var min = Math.floor(sec / 60);
+        sec %= 60;
+        return this.strPaddingLeft(min.toString(), 2, '0') + ':' + this.strPaddingLeft(sec.toString(), 2, '0');
+    }
+    numLimit(num:number, min: number, max: number) {
+        return (num < min || typeof num != 'number' || isNaN(num)) ? min :
+                (num > max) ? max : num;
+    }
+    Timer = class {
+        callback: () => void;
+        cancelFunc: () => void;
+        constructor(callback: () => void) {
+            this.callback = callback;
+        }
+        timeout(time) {
+            var handle = setTimeout(this.callback, time);
+            this.cancelFunc = () => window.clearTimeout(handle);
+        }
+        interval(time) {
+            var handle = setInterval(this.callback, time);
+            this.cancelFunc = () => window.clearInterval(handle);
+        }
+        tryCancel() {
+            if (this.cancelFunc) this.cancelFunc();
+        }
+    }
 }
 
 var ui = {
-    bottomBar: {
-        container: document.getElementById("bottombar"),
+    bottomBar: new class {
+        container: HTMLElement = document.getElementById("bottombar");
+
         toggle(state?: boolean) {
-            helpers.toggleClass(ui.bottomBar.container, 'show', state);
-        },
+            helpers.toggleClass(this.container, 'show', state);
+        }
         init() {
-            var bar = ui.bottomBar.container;
-            bar.addEventListener('mouseenter', () => {
-                ui.bottomBar.toggle(true);
+            var bar = this.container;
+            var hideTimer = new helpers.Timer(() => {
+                this.toggle(false);
             });
-            bar.addEventListener('mouseleave', ()=>{
-                ui.bottomBar.toggle(false);
+            bar.addEventListener('mouseenter', () => {
+                hideTimer.tryCancel();
+                this.toggle(true);
+            });
+            bar.addEventListener('mouseleave', () => {
+                hideTimer.tryCancel();
+                hideTimer.timeout(200);
             });
         }
     },
-    progressBar: {
-        container: document.getElementById('progressbar'),
-        fill: document.getElementById('progressbar-fill'),
-        setProg(prog: number) {
-            if (typeof prog != 'number' || prog < 0) prog = 0;
-            else if (prog > 1) prog = 1;
-            ui.progressBar.fill.style.width = (prog * 100) + '%';
+    progressBar: new class {
+        container = document.getElementById('progressbar');
+        fill = document.getElementById('progressbar-fill');
+        labelCur = document.getElementById('progressbar-label-cur');
+        labelTotal = document.getElementById('progressbar-label-total');
+
+        setProg(cur: number, total: number) {
+            var prog = cur / total;
+            prog = helpers.numLimit(prog, 0, 1);
+            this.fill.style.width = (prog * 100) + '%';
+            this.labelCur.textContent = helpers.formatTime(cur);
+            this.labelTotal.textContent = helpers.formatTime(total);
+        }
+        setProgressChangedCallback(cb: (percent: number) => void) {
+            var call = (e) => { cb(helpers.numLimit(e.offsetX / this.container.clientWidth, 0, 1)); }
+            this.container.addEventListener('mousedown', (e) => {
+                call(e);
+            });
+            this.container.addEventListener('mousemove', (e) => {
+                if (e.buttons == 1) call(e);
+            });
         }
     }
 };
@@ -40,11 +94,17 @@ class PlayerCore {
     constructor() {
         this.audio = document.createElement('audio');
         this.audio.addEventListener('timeupdate', (e) => {
-            ui.progressBar.setProg(this.audio.currentTime / this.audio.duration);
+            ui.progressBar.setProg(this.audio.currentTime, this.audio.duration);
+        });
+        ui.progressBar.setProgressChangedCallback((x) => {
+            this.audio.currentTime = x * this.audio.duration;
         });
     }
-    playUrl(src: string) {
+    loadUrl(src: string) {
         this.audio.src = src;
+    }
+    playUrl(src: string) {
+        this.loadUrl(src);
         this.audio.play();
     }
     play() {
@@ -56,3 +116,4 @@ class PlayerCore {
 }
 
 var playerCore = new PlayerCore();
+playerCore.loadUrl('test.mp3');
