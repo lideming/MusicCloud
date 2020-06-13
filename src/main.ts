54e8ed16b99c5cece26e41e9ccbae27b4294ea95
lideming <14901890+lideming@users.noreply.github.com>
2020-06-13 13:35:14 +0800

    add missing tslib functions

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@
 
 // Why do we need to use React and Vue.js? ;)
 
+//@ts-ignore
+const __createBinding = globalThis.__createBinding = Object.create ? (function(o, m, k, k2?) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+});
+
 globalThis.__awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
 	function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
 	return new (P || (P = Promise))(function (resolve, reject) {
@@ -11,6 +20,9 @@ globalThis.__awaiter = (this && this.__awaiter) || function (thisArg, _arguments
 		function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
 		step((generator = generator.apply(thisArg, _arguments || [])).next());
 	});
+};
+globalThis.__exportStar = function(m, exports) {
+    for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 
 console.time('main init');
