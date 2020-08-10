7469bfefbf44f6fdac3504d7779fe55a943e0978
lideming <14901890+lideming@users.noreply.github.com>
2020-08-11 06:08:26 +0800

    generate minified bundle with sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
+import { terser } from "rollup-plugin-terser";
 
 const rollupConfig = () => ({
     input: './src/main.ts',
     output: {
         file: './bundle.js',
         format: 'umd',
-        name: 'mcloud'
+        name: 'mcloud',
+        plugins: [terser()],
+        sourcemap: true,
+        sourcemapExcludeSources: true,
+        sourcemapPathTransform: transformSourcemapPath()
     },
     plugins: [
         buildInfo(),
@@ -40,6 +45,21 @@ function buildInfo() {
             }
             return null;
         }
+    };
+}
+
+function transformSourcemapPath() {
+    return (rel, path) => {
+        rel = rel.replace(/\\/g, '/');
+        if (rel.startsWith('node_modules')) {
+            var mat = rel.match(/^node_modules\/((?:@[\w\-_]+\/)?[\w\-_]+)\/(.*)$/);
+            if (!mat) {
+                console.warn(['sourcemapPathTransform', rel]);
+            }
+            var version = require(mat[1] + '/package.json').version;
+            return `https://cdn.jsdelivr.net/npm/${mat[1]}@${version}/${mat[2]}`;
+        }
+        return `https://github.com/lideming/MusicCloud/raw/dev/${rel}`;
     };
 }
 
