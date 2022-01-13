c3f1a044acf61635771699e3fa084e5ddb96e81f
lideming <14901890+lideming@users.noreply.github.com>
2022-01-14 06:15:29 +0800

    dont register service worker for non-https

--- src/ServiceWorker/client.ts
+++ src/ServiceWorker/client.ts
@@ -1,11 +1,15 @@
 export const serviceWorkerClient = new class ServiceWorkerClient {
     reg: ServiceWorkerRegistration | null = null;
     async init() {
-        try {
-            this.reg = await navigator.serviceWorker.register('sw.bundle.js', { scope: '.' });
-            console.info('[sw client] service worker registered', this.reg);
-        } catch (error) {
-            console.error('[sw client] error', error);
+        if (window.location.protocol == 'https:') {
+            try {
+                this.reg = await navigator.serviceWorker.register('sw.bundle.js', { scope: '.' });
+                console.info('[sw client] service worker registered', this.reg);
+            } catch (error) {
+                console.error('[sw client] error', error);
+            }
+        } else {
+            console.info('[sw client] non-https protocol, skipping service worker registration');
         }
     }
 };
