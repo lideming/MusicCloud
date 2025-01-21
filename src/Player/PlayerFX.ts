2f867cfe50ee2a136a8947be907b80924101c99b
lideming <14901890+lideming@users.noreply.github.com>
2025-01-22 03:56:32 +0800

    feat: WebAudio experiments

--- src/Player/PlayerFX.ts
+++ src/Player/PlayerFX.ts
@@ -1,4 +1,4 @@
-import { Dialog, Func, View } from "../Infra/viewlib";
+import { Dialog, Func, Timer, View } from "../Infra/viewlib";
 import { playerCore } from "./PlayerCore";
 
 export const playerFX = new (class PlayerFX {
@@ -6,11 +6,12 @@ export const playerFX = new (class PlayerFX {
   source: MediaElementAudioSourceNode;
   analyser: AnalyserNode;
   gainNode: GainNode;
+  lastBuffers: Array<Array<Float32Array>> = []; // [history blocks (from old to new)][channel][sample index] -> sample value
   get webAudioInited() {
     return !!this.ctx;
   }
   init() {}
-  initWebAudio() {
+  async initWebAudio() {
     if (this.webAudioInited) return;
     console.info("[PlayerFX] switching to WebAudio");
     this.ctx = new AudioContext({ latencyHint: "playback" });
@@ -28,11 +29,60 @@ export const playerFX = new (class PlayerFX {
     // iOS background playing fix
     this.ctx.addEventListener("statechange", (e) => {
       console.info("[PlayerFX] audio ctx state", this.ctx.state);
-      if (this.ctx.state === "interrupted" as any) {
+      if (
+        this.ctx.state === ("interrupted" as any) &&
+        playerCore.state === "playing"
+      ) {
+        console.info("[PlayerFX] try resuming");
         this.ctx.resume();
+        this.ctx.sampleRate
       }
     });
+    playerCore.onStateChanged.add(() => {
+      if (playerCore.state === "playing") {
+        this.suspendTimer.tryCancel();
+        if (this.ctx.state !== "running") {
+          this.ctx.resume();
+        }
+      } else if (
+        playerCore.state === "paused" &&
+        this.ctx.state === "running"
+      ) {
+        this.suspendTimer.timeout(1000); // wait for fading effect
+      }
+    });
+
+    const audioModule = () => {
+      //@ts-ignore
+      class AudioCollector extends (AudioWorkletProcessor as any) {
+        process(inputs, outputs, parameters) {
+          this.port.postMessage([...inputs[0]]);
+          return true;
+        }
+      }
+
+      //@ts-ignore
+      registerProcessor("audio-collector", AudioCollector);
+    };
+
+    await this.ctx.audioWorklet.addModule(
+      URL.createObjectURL(
+        new Blob([`(${audioModule.toString()})()`], {
+          type: "application/javascript",
+        })
+      )
+    );
+
+    const workletNode = new AudioWorkletNode(this.ctx, "audio-collector");
+    this.gainNode.connect(workletNode);
+    workletNode.port.onmessage = (ev) => {
+      this.lastBuffers.push(ev.data);
+      if (this.lastBuffers.length > 10) {
+        this.lastBuffers.shift();
+      }
+    };
   }
+  suspendTimer = new Timer(() => this.ctx.suspend());
   showUI(ev?: MouseEvent) {
     this.initWebAudio();
     new FXDialog().show(ev);
@@ -40,9 +90,12 @@ export const playerFX = new (class PlayerFX {
 })();
 
 const WIDTH = 800;
-const HEIGHT = 600;
+const HEIGHT = 300;
+
+const lines = true;
 
 class FXDialog extends Dialog {
+  title = "WebAudio";
   canvas = new View<HTMLCanvasElement>({
     tag: "canvas",
     height: HEIGHT,
@@ -76,17 +129,65 @@ class FXDialog extends Dialog {
       history.unshift(freq);
       if (history.length > HEIGHT) history.pop();
 
-      const buf = imgBuffer;
-      for (let y = 0; y < history.length; y++) {
-        for (let x = 0; x < WIDTH; x++) {
-          const val = history[y][x];
-          const bufIdx = 4 * (x + WIDTH * (HEIGHT - y - 1));
-          buf[bufIdx + 0] = buf[bufIdx + 1] = buf[bufIdx + 2] = val;
-          buf[bufIdx + 3] = 255;
-        }
-      }
+      const buffers = playerFX.lastBuffers;
+      if (!buffers.length) return;
+      const totalSamples = buffers.reduce((a, b) => a + b[0].length, 0);
 
-      ctx.putImageData(imgData, 0, 0);
+      if (lines) {
+        ctx.clearRect(0, 0, WIDTH, HEIGHT);
+        for (let ch = 0; ch < 2; ch++) {
+          let bufferIndex = 0;
+          let sampleIndex = 0;
+          ctx.strokeStyle = ch ? "blue" : "red";
+          ctx.beginPath();
+          for (let i = 0; i < totalSamples; i++) {
+            const sample = buffers[bufferIndex][ch][sampleIndex];
+            const x = (i / totalSamples) * WIDTH;
+            const y = Math.floor((sample / 2) * HEIGHT + HEIGHT / 2);
+            if (!bufferIndex && !sampleIndex) {
+              ctx.moveTo(x, y);
+            }
+            ctx.lineTo(x, y);
+            // buf[bufIdx + 3] = 255;
+            if (sampleIndex + 1 < buffers[bufferIndex][ch].length) {
+              sampleIndex++;
+            } else if (bufferIndex + 1 < buffers.length) {
+              bufferIndex++;
+              sampleIndex = 0;
+            } else {
+              break;
+            }
+          }
+          ctx.stroke();
+        }
+      } else {
+        const buf = imgBuffer;
+        for (let i = 0; i < buf.length; i++) {
+          buf[i] = 255;
+        }
+        for (let ch = 0; ch < 2; ch++) {
+          let bufferIndex = 0;
+          let sampleIndex = 0;
+          for (let x = 0; x < WIDTH; x++) {
+            const sample = buffers[bufferIndex][ch][sampleIndex];
+            const y = Math.floor((sample / 2) * HEIGHT + HEIGHT / 2);
+            const bufIdx = 4 * (x + WIDTH * (HEIGHT - y - 1));
+            buf[bufIdx + 1] -= 127;
+            buf[bufIdx + (ch ? 0 : 2)] = 0;
+            // buf[bufIdx + 3] = 255;
+            if (sampleIndex + 1 < buffers[bufferIndex][ch].length) {
+              sampleIndex++;
+            } else if (bufferIndex + 1 < buffers.length) {
+              bufferIndex++;
+              sampleIndex = 0;
+            } else {
+              break;
+            }
+          }
+        }
+
+        ctx.putImageData(imgData, 0, 0);
+      }
     };
 
     requestAnimationFrame(update);
